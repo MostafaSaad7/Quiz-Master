8380b0f87a6df90770783e5b8c3a532510829297
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2021-12-06 10:18:19 +0200

    SwappingSprites

--- Quiz Master/Assets/Scripts/Quiz.cs
+++ Quiz Master/Assets/Scripts/Quiz.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
     {
@@ -10,20 +11,54 @@ public class Quiz : MonoBehaviour
     [SerializeField] QuestionSO question;
 
     [SerializeField] GameObject [] answersButtons;
+
+    [SerializeField] int correctAnswerIndex;
+
+    [SerializeField] Sprite defaultAnswerSprite;
+
+   [SerializeField] Sprite correctAnswerSprite;
     void Start()
     {
     
     questionText.text=question.GetQuestion();
 
     for(int i=0 ;i< 4 ;i++)
+    
     {
-  
         TextMeshProUGUI buttonText=answersButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-        buttonText.text=question.GetAnswer(i);
-
+        buttonText.text=question.GetAnswer(i); 
     }
 
         
+    }
+
+
+    public void onAnswerSelected (int index)
+    {
+
+        if(index==question.GetCorrectAnswerIndex())
+        {   
+            questionText.text="Correct!!";
+            Image buttonImage = answersButtons[index].GetComponent<Image>();
+            buttonImage.sprite=correctAnswerSprite;
+
+
+
+        }
+
+
+        else 
+        {
+            correctAnswerIndex=question.GetCorrectAnswerIndex();
+
+            questionText.text="Sorry the correct answer was; \n"+question.GetAnswer(correctAnswerIndex);
+
+            Image buttonImage=answersButtons[correctAnswerIndex].GetComponent<Image>();
+
+            buttonImage.sprite=correctAnswerSprite;
+
+        }
+
     }
 
 }
