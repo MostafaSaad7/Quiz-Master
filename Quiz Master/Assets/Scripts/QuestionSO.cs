4dd09c3f13b410cf9a48b6c792da426399d32b0b
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2021-12-06 08:59:58 +0200

    Arrays

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -9,15 +9,32 @@ public class QuestionSO : ScriptableObject
 
  //minimun 2 lines && max of 6 lines    
  [TextArea(2,6)]   
-[SerializeField]string question="Enter new question text here";
+[SerializeField] string question="Enter new question text here";
+[SerializeField] string [] answers =new string[4] ;
+[SerializeField]int correctAnswerIndex;
 
 
-public string getQuestion()
+public string GetQuestion()
 {
 
 return question;
 
 
 } 
+
+public int  GetCorrectAnswerIndex()
+{
+
+    return correctAnswerIndex;
+
+}
+
+public string GetAnswer (int index)
+{
+
+return answers[index];
+
+}
+
 
 }
